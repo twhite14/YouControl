3e23e78fb503dafc39e2a10936f8d4402581743e
nathan206100 <nathansteward13@gmail.com>
2017-04-28 12:36:02 -0400

    added final code used for demo
    initial_dongle_sketch is for the usb side
    initial_controller_sketch is for the controller with psp style joystick
    initial_controller_sketch_largejs is for the controller with 360-style joystick

---
+++ code_arduino_rev1/initial_controller_sketch/initial_controller_sketch.ino
@@ -0,0 +1,151 @@
+#include <SPI.h>
+#include <Wire.h>
+#include "RF24.h"
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+/*DEFINE SETUPS*/
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+/*PIN SETUPS*/
+int ButtonA = 8;
+int ButtonB = 9;
+int ButtonX = 5;
+int ButtonY = 10;
+int ButtonStart = 6;//gotta define these after final implementation
+int ButtonSelect = 12;
+int ButtonRbump = A2;
+int ButtonLbump = A3;
+int Xaxis = A1;
+int Yaxis = A0;
+int count = 0;
+bool cntrl_assign = false;
+RF24 radio(7,11);
+byte addresses[][6] = {"1Node","2Node"}; //first is check pipe second is feedback
+long pipeassign;
+long getassign;
+/*setup======================================================*/
+void setup() {
+//start lcd
+Serial.begin(115200);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.display();
+  delay(4000);
+  display.clearDisplay();
+//start radio
+  radio.begin();//start it
+  radio.setDataRate(RF24_2MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(addresses[1]);
+  radio.openReadingPipe(1,addresses[0]);//open check pipe
+  radio.startListening();//start listening on the pipe
+//button setup
+  pinMode(ButtonA,INPUT);
+  pinMode(ButtonB,INPUT);
+  pinMode(ButtonY,INPUT);
+  pinMode(ButtonX,INPUT);
+}
+/*loop=======================================================*/
+long selector = 1;
+void loop() {
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+  while(!cntrl_assign){//while not assigned, check for assignment value
+      delay(100);
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0,16);
+      display.println("start to change #,");
+      display.setCursor(0,26);
+      display.println("select to confirm :");
+      
+        //cntrl_assign = true;
+      
+      if(digitalRead(ButtonStart) == LOW){
+        selector++;
+        if (selector == 5){selector = 1;}
+      }
+      if (selector == 1){
+        display.setCursor(0,0);
+        display.println("[Controller 1]");
+      }
+      else if (selector == 2){
+        display.setCursor(0,0);
+        display.println("[Controller 2]");
+      }
+      else if (selector == 3){
+        display.setCursor(0,0);
+        display.println("[Controller 3]");
+      }
+      else if (selector == 4){
+        display.setCursor(0,0);
+        display.println("[Controller 4]");
+      }
+      display.display();
+      if(digitalRead(ButtonSelect) == LOW){
+         cntrl_assign = true;
+      }
+    }
+  long dispnum = selector - 1;
+  radio.stopListening();
+  radio.openWritingPipe(addresses[1]);
+  
+  while(cntrl_assign){
+    unsigned long sendmsg = statusreturn(dispnum);
+    radio.write(&sendmsg,sizeof(unsigned long));
+    delay(10);
+    if(count>=20){
+  //    update screen stuff
+  //    check battery levels
+    
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(WHITE);
+      display.setCursor(0,30);
+      display.println("ctrl #");
+      display.setTextSize(2);
+      display.setCursor(20,40);
+      display.print(selector,DEC);
+      display.display();
+      count = 0;
+    }
+    count++;
+  }
+  
+}
+/*statusreturn===============================================*/
+long statusreturn(long cntrlnum){ //status of all states
+  long stat = 0;
+  long a     = digitalRead(ButtonA);
+  long b     = digitalRead(ButtonB);
+  long x     = digitalRead(ButtonX);
+  long y     = digitalRead(ButtonY);
+  long sta   = digitalRead(ButtonStart);
+  long sel   = digitalRead(ButtonSelect);
+  long rbump = digitalRead(ButtonRbump);
+  long lbump = digitalRead(ButtonLbump);
+  long xax   = analogRead(Xaxis)/4;
+  long yax   = analogRead(Yaxis)/4;
+
+  //OR the bitshifted states to make 4 byte state
+  //DIO 0-7B 
+  //Analog 8-15B, 16-23B
+  //controller number 24-31B
+  stat |= (a); 
+  stat |= (b << 1);
+  stat |= (x << 2);
+  stat |= (y << 3);
+  stat |= (sta << 4); 
+  stat |= (sel << 5);
+  stat |= (rbump << 6);
+  stat |= (lbump << 7);
+  stat |= (xax << 8);
+  stat |= (yax << 16);
+  stat |= (cntrlnum << 24);
+  Serial.println(stat,HEX);
+  
+  return stat;
+}//end statusreturn
---
+++ code_arduino_rev1/initial_dongle_sketch/initial_dongle_sketch.ino
@@ -0,0 +1,98 @@
+#include <SPI.h>
+#include <Wire.h>
+#include "RF24.h"
+#include "Joystick.h"
+
+RF24 radio(7,11);
+byte addresses[][6] = {"1Node","2Node"}; //first is check pipe second is feedback
+byte pipeassign = 0;
+
+#define NUM_BUTTONS   8
+//Dongle joystick config definitions
+#define JOY0_ID         3
+#define JOY1_ID         4
+#define JOY2_ID         5
+#define JOY3_ID         6
+#define JOY_TYPE        JOYSTICK_TYPE_GAMEPAD
+#define HAT_SWITCHES    0
+#define HAS_X_AXIS      true
+#define HAS_Y_AXIS      true
+#define HAS_Z_AXIS      false
+#define HAS_RX_AXIS     false
+#define HAS_RY_AXIS     false
+#define HAS_RZ_AXIS     false
+#define HAS_RUDDER      false
+#define HAS_THROTTLE    false
+#define HAS_ACCELERATOR false
+#define HAS_BRAKE       false
+#define HAS_STEERING    false
+
+#define NUM_JOYSTICKS   4
+
+//Joystick objects
+Joystick_ Joystick[] = {
+  Joystick_(JOY0_ID, JOY_TYPE, NUM_BUTTONS, HAT_SWITCHES, HAS_X_AXIS, HAS_Y_AXIS, HAS_Z_AXIS, HAS_RX_AXIS, HAS_RY_AXIS, HAS_RZ_AXIS, HAS_RUDDER, HAS_THROTTLE, HAS_ACCELERATOR, HAS_BRAKE, HAS_STEERING),
+  Joystick_(JOY1_ID, JOY_TYPE, NUM_BUTTONS, HAT_SWITCHES, HAS_X_AXIS, HAS_Y_AXIS, HAS_Z_AXIS, HAS_RX_AXIS, HAS_RY_AXIS, HAS_RZ_AXIS, HAS_RUDDER, HAS_THROTTLE, HAS_ACCELERATOR, HAS_BRAKE, HAS_STEERING),
+  Joystick_(JOY2_ID, JOY_TYPE, NUM_BUTTONS, HAT_SWITCHES, HAS_X_AXIS, HAS_Y_AXIS, HAS_Z_AXIS, HAS_RX_AXIS, HAS_RY_AXIS, HAS_RZ_AXIS, HAS_RUDDER, HAS_THROTTLE, HAS_ACCELERATOR, HAS_BRAKE, HAS_STEERING),
+  Joystick_(JOY3_ID, JOY_TYPE, NUM_BUTTONS, HAT_SWITCHES, HAS_X_AXIS, HAS_Y_AXIS, HAS_Z_AXIS, HAS_RX_AXIS, HAS_RY_AXIS, HAS_RZ_AXIS, HAS_RUDDER, HAS_THROTTLE, HAS_ACCELERATOR, HAS_BRAKE, HAS_STEERING),
+  };
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  for(int index=0;index<NUM_JOYSTICKS;index++){
+    Joystick[index].begin(true);
+    Joystick[index].setXAxisRange(255, 0);
+    Joystick[index].setYAxisRange(255, 0);
+  }
+  radio.begin();//start it
+  radio.setDataRate(RF24_2MBPS);
+  radio.setPALevel(RF24_PA_MAX);
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);//open check pipe
+  radio.startListening();//start listening on the pipe
+}
+unsigned long gotmsg = 0x0;
+
+void loop() {
+  
+  if(radio.available()){
+    while(radio.available()){
+    radio.read(&gotmsg, sizeof(unsigned long));
+    uint8_t controller_num = (gotmsg >> 24) & 0xFF;
+    uint8_t button_states = (gotmsg) & 0xFF;
+    uint8_t xax = 0xFF&(gotmsg>>8);
+    uint8_t yax = 0xFF&(gotmsg>>16);
+    Serial.println(gotmsg,HEX);
+    Serial.print("controller number:  ");
+    Serial.println(controller_num,HEX);
+    Serial.print("button states:      ");
+    Serial.println(button_states,HEX);
+    Serial.print("x axis value:       ");
+    Serial.println(xax,HEX);
+    Serial.print("y axis value:       ");
+    Serial.println(yax,HEX);
+    Joystick[controller_num].setXAxis(xax);
+    Joystick[controller_num].setYAxis(yax);
+    writeDButtons(controller_num, button_states);
+    }
+  }
+}
+
+//Button pins in order
+void writeDButtons(uint8_t controller, uint16_t states)
+{
+  for(int i = 0; i < NUM_BUTTONS; i++)
+  {
+    if(((states>>i) & 1) == 0)
+    {
+      Joystick[controller].pressButton(i);
+    }
+    else if(((states>>i) & 1) == 1)
+    {
+      Joystick[controller].releaseButton(i);
+    }
+  }
+  return states;
+}
+
